c989fb45f32dfc89da238a318a95a52243c6f625
SattoByte <sattobyte@gmail.com>
2024-07-25 15:01:51 -0400

    Seted up work space

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      secon git test
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg />
+          <div className="headerInfo">
+            <h3>React Snickers</h3>
+            <p>Best sneakers store</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <svg />
+            <span>200$</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>
+          All sneakers
+        </h1>
+        ...
+      </div>
     </div>
   );
 }
